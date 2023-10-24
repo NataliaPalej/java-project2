dfa11168b5d3e5fd41e4b380febe07fc7b851785
Natalia <PalejNatalia@gmail.com>
2023-10-24 14:37:34 +0100

    Moved phones into List, finished Part1

--- src/project2/TestPhone.java
+++ src/project2/TestPhone.java
@@ -4,88 +4,104 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TestPhone {
-	
-//	static ArrayList<Phone> phoneList = new ArrayList<>();
 
 	public static void main(String[] args) {
 		
-		Phone p1 = new Phone("Sony", "Experia X", 32, 12.5, 3.6, "Yes", 150);
-		Phone p2 = new Phone("Sony", "Experia Z", 64, 14.2, 5.6, "Yes" ,175);
-		Phone p3 = new Phone("Samsung", "Galaxy M", 64, 14.5, 5.4, "Yes", 180);
-		Phone p4 = new Phone("Nokia", "3330", 16, 13.2, 2.3, "No", 90);
-		Phone p5 = new Phone("Motorola", "M1", 8, 11.3, 4.9, "Yes", 100);
-		Phone p6 = new Phone("iPhone", "6", 32, 13.5, 6.4, "Yes", 250);
-		Phone p7 = new Phone("Alcatel", "A3", 8, 9.3, 2.4, "No", 50);
+		// Create array to store phones in
+		List<Phone> phones = new ArrayList<>();
 		
-//		phoneList.add(p1);
-//		phoneList.add(p2);
-//		phoneList.add(p3);
-//		phoneList.add(p4);
-//		phoneList.add(p5);
-//		phoneList.add(p6);
-//		phoneList.add(p7);
+		// Add phones from the table
+		phones.add(new Phone("Sony", "Experia X", 32, 12.5, 3.6, "Yes", 150));
+		phones.add(new Phone("Sony", "Experia Z", 64, 14.2, 5.6, "Yes" ,175));
+		phones.add(new Phone("Samsung", "Galaxy M", 64, 14.5, 5.4, "Yes", 180));
+		phones.add(new Phone("Nokia", "3330", 16, 13.2, 2.3, "No", 90));
+		phones.add(new Phone("Motorola", "M1", 8, 11.3, 4.9, "Yes", 100));
+		phones.add(new Phone("iPhone", "6", 32, 13.5, 6.4, "Yes", 250));
+		phones.add(new Phone("Alcatel", "A3", 8, 9.3, 2.4, "No", 50));
 		
-//		System.out.println("Amount of items in array: " + phoneList.size());
-//		System.out.println();
-		
-		// Serialize
 		try {
-			// Serialize to the file
-			FileOutputStream fileOut = new FileOutputStream("phone.ser");
-			// Assign what object to serialize
-			@SuppressWarnings("resource")
-			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
-			objOut.writeObject(p1);
-			objOut.writeObject(p2);
-			objOut.writeObject(p3);
-			objOut.writeObject(p4);
-			objOut.writeObject(p5);
-			objOut.writeObject(p6);
-			objOut.writeObject(p7);
-			
-			System.out.println("SERIALIZED:\n\n" + p1.printDetails() + "\n" + p2.printDetails() + "\n" + p3.printDetails() + "\n" + 
-								p4.printDetails() + "\n" + p5.printDetails() + "\n" + p6.printDetails() + "\n" +  p7.printDetails() );
-			
-			Phone p8 = new Phone("Huawei", "P30 Pro", 128, 14.5, 5.4, "Yes", 200);
-			System.out.println("\nNew phone added: \n" + p8.printDetails());
-			objOut.writeObject(p8);
-			System.out.println("\nNew phone serialized: " + p8.getMake());
+			serialisePhones(phones);
+			System.out.println("Phones successfully serialised.\n");
 		} catch (Exception e) {
-			System.out.println("Exception cought.\n");
+			System.out.println("Couldn't serialize.\nError: ");
 			e.printStackTrace();
 		}
 		
 		System.out.println("\n--------------------------------------------------------------------------------------------------------------------\n");
 		
-		// Deserialize
+		// Deserializing
+		System.out.println("Deserialised:\n");
+		// Create list for deserialization 
+		List<Phone> deserializedPhones = deserialisePhones();
+		for (Phone phone : deserializedPhones) {
+			System.out.println(phone.printDetails());
+		}
+		
+		// Print message phone successfully deleted
+		System.out.println("\nDeleting Alcatel... \n" + phones.remove(6).getMake() + " successfully deleted.");
+		
+
+		// Check if phone was deleted
+		try {
+			System.out.println(phones.get(6).getMake());
+		} catch (Exception e) {
+			System.out.println("\nChecking if phone still in table...\nPhone doens't exist in the table.");
+		}
+		
+		// Add new phone to the list
+		phones.add(new Phone("Huawei", "P30 Pro", 128, 14.5, 5.4, "Yes", 250));
+		System.out.println("\nNew phone has been added: \n" + phones.get(6).printDetails());
+	}
+	
+	// Serialise Method 
+	public static void serialisePhones(List<Phone> phones) {
+		
+		try {
+			// Serialise to the file
+			FileOutputStream fileOut = new FileOutputStream("phone.ser");
+			// Assign what object to serialise
+			@SuppressWarnings("resource")
+			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+			// Loop through phones array
+			for (Phone p : phones) {
+                objOut.writeObject(p);
+            }
+			objOut.close();
+            fileOut.close();
+		} catch (Exception e) {
+			System.out.println("Exception cought.\n");
+			e.printStackTrace();
+		}
+	}
+	
+	// Deserialise method
+	public static List<Phone> deserialisePhones() {
+		List<Phone> phones = new ArrayList<>();
 		try {
 			FileInputStream fileIn = new FileInputStream("phone.ser");
 			ObjectInputStream objIn = new ObjectInputStream(fileIn);
-			Phone phone1 = (Phone)objIn.readObject();
-			Phone phone2 = (Phone)objIn.readObject();
-			Phone phone3 = (Phone)objIn.readObject();
-			Phone phone4 = (Phone)objIn.readObject();
-			Phone phone5 = (Phone)objIn.readObject();
-			Phone phone6 = (Phone)objIn.readObject();
-			Phone phone7 = (Phone)objIn.readObject();
 			
-			System.out.println("DESERIALIZED:\n\n" + phone1.printDetails() + "\n" + phone2.printDetails() 
-								+ "\n" + phone3.printDetails() + "\n" + phone4.printDetails() + "\n" + phone5.printDetails() 
-								+ "\n" + phone6.printDetails() + "\n" + phone7.printDetails());
-			
-			Phone phone8 = (Phone)objIn.readObject();
-			System.out.println("\nDESERIALIZING NEWLY ADDED PHONE:");
-			System.out.println(phone8.printDetails());
-			
-			
+			// Loop until end of file is reached
+			while (true) {
+                try {
+                    Phone p = (Phone) objIn.readObject();
+                    phones.add(p);
+                } catch (Exception e) {
+                	//System.out.println("\n# End of File Reached#\n");
+                    break; 
+                }
+            }
 			objIn.close();
 			fileIn.close();
 		} catch (Exception e) {
 			System.out.println("Couldn't deserialize the file!");
+			e.printStackTrace();
 		}
+		return phones;
 	}
 }
